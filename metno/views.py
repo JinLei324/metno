3ed9c8e006696328ee90021c73ffc04131eedbb3
JinLei <xiaojinteacher@gmail.com>
2021-05-31 18:32:42 +0800

    call api

--- metno/views.py
+++ metno/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.template import loader
-# Create your views here.
-from django.http import HttpResponse
+import requests
 
 
 def index(request):
+    headers = {'user-agent': 'metno/0.0.1'}
+    response = requests.get("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=51.5&lon=-0.25", headers=headers)
     
     context = {
-        'a': 'sample',
+        'a': 'sample'
     }
     return render(request, 'index.html', context)
